c040ca179ff140f6596c9909c5bdf55e20f5b2cb
meghaherur <megha.herur@gmail.com>
2017-10-30 19:03:44 -0700

    Add files via upload

--- media.py
+++ media.py
@@ -1,19 +1,22 @@
-
 import webbrowser
 
+
 class Movie_Parent():
-    def __init__(self,movie_title,run_time):
-      self.title=movie_title
-      self.running_time=run_time
+    """This is a parent class"""
+    def __init__(self, movie_title, run_time):  # Class Constructor
+        self.title = movie_title
+        self.running_time = run_time
+
 
 class Movie(Movie_Parent):
-    """ This is a movie class to capture all details about movie""" 
-   
-    def __init__(self,movie_title,run_time,poster_image,trailer_youtube):
-       Movie_Parent.__init__(self,movie_title,run_time)
-       self.poster_image_url = poster_image
-       self.trailer_youtube_url = trailer_youtube
+    """ This is a movie class to capture all details about movie"""
+    """ This is a child Class"""
 
-    def show_trailer(self):
-       webbrowser.open(self.trailer_youtube_url)
-        
+    def __init__(self, movie_title, run_time,
+                 poster_image, trailer_youtube):  # Child constructor
+        Movie_Parent.__init__(self, movie_title, run_time)
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):  # Class Method
+        webbrowser.open(self.trailer_youtube_url)
